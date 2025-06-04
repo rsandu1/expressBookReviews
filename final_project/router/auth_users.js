e1efcda0f7bbc1a8cc919d4c0a3987d9063704c5
rsandu1 <rsandu1@fordham.edu>
2025-06-04 12:31:27 -0400

    updated user routes

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,7 +3,16 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [ 
+    {
+        "username": "user1",
+        "password": "test1"
+    },
+    {
+        "username": "user2",
+        "password": "test2"
+    }
+];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
